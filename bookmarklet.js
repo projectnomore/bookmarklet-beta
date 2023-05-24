3277e6efca4013a2a352cf70bd1963bdfd9760b0
paxtrain55 <119540345+disnos9@users.noreply.github.com>
2023-05-24 15:18:09 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,10 +5,11 @@ javascript:(function() {
   container.innerHTML = `
     <p id="message">Press Shift + ] to close or open</p>
     <h1>Made By ProjectNoMore</h1>
+    <h2>Error: Injection currently not working.
     <hr>
-    <h2>Edpuzzle by <a href="https://www.librecheats.net/">LibreCheats</a></hr>
-    <a href="https://ujosd1.csb.app/hacks/edpuzzle.html">View on PNM Site</a>
-    <button><a href="javascript: document.dev_env = 'https://edpuzzle-beta.hs.vc'; fetch('https://edpuzzle-beta.hs.vc/script.js').then(r => r.text()).then(r => eval(r))">Inject</a></button>
+    <!--<h2>Edpuzzle by <a href="https://www.librecheats.net/">LibreCheats</a></hr>
+    <a href="https://ujosd1.csb.app/hacks/edpuzzle.html">View on PNM Site</a>-->
+    
     
   `;
 
