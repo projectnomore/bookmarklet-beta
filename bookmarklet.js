fe6ce402e0a7e6a724b164a8e884daacd2a28707
paxtrain55 <119540345+disnos9@users.noreply.github.com>
2023-05-24 15:22:01 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,17 +1,44 @@
 javascript:(function() {
   // Create a container div for the UI
   var container = document.createElement('div');
-  container.setAttribute('id', 'container');
-  container.innerHTML = `
-    <p id="message">Press Shift + ] to close or open</p>
-    <h1>Made By ProjectNoMore</h1>
-    <h2>Error: Injection currently not working.
-    <hr>
-    <!--<h2>Edpuzzle by <a href="https://www.librecheats.net/">LibreCheats</a></hr>
-    <a href="https://ujosd1.csb.app/hacks/edpuzzle.html">View on PNM Site</a>-->
-    
-    
-  `;
+  container.style.position = 'fixed';
+  container.style.top = '20px';
+  container.style.left = '20px';
+  container.style.padding = '10px';
+  container.style.background = 'rgba(0, 0, 0, 0.5)';
+  container.style.border = '1px solid black';
+  container.style.zIndex = '9999';
+
+  // Calculate the dimensions for 2.5/4ths of the screen
+  var screenWidth = window.innerWidth;
+  var screenHeight = window.innerHeight;
+  var containerWidth = (screenWidth / 4) * 2.5;
+  var containerHeight = (screenHeight / 4) * 2.5;
+
+  // Set the dimensions for the container
+  container.style.width = containerWidth + 'px';
+  container.style.height = containerHeight + 'px';
+
+  // Create a message indicating how to close the UI
+  var message = document.createElement('p');
+  message.textContent = 'Press Shift + ] to close or open';
+  message.style.color = '#fff';
+  message.style.fontSize = '24px';
+  message.style.fontWeight = 'bold';
+  message.style.textAlign = 'center';
+  container.appendChild(message);
+
+  // Create a heading element
+  var heading = document.createElement('h1');
+  heading.textContent = 'Made By ProjectNoMore';
+  heading.style.color = 'red';
+  heading.style.fontSize = '32px';
+  heading.style.fontWeight = 'bold';
+  heading.style.textAlign = 'center';
+  heading.style.marginTop = '20px';
+
+  // Append the heading to the container
+  container.appendChild(heading);
 
   // Append the container to the document body
   document.body.appendChild(container);
@@ -28,7 +55,3 @@ javascript:(function() {
     }
   });
 })();
-// PLEASE BE AWARE OF THE FOLLOWING NOTICE:
-/*              NOTICE                  */
-/*   PNM EXECUTER (c) 2023 PNM.         */
-/*              NOTICE                  */
